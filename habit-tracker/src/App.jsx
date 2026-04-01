77e2d5886648fef001df79873de29b3051d9eef6
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2026-04-01 19:18:58 +0530

    adding sterak feature

--- habit-tracker/src/App.jsx
+++ habit-tracker/src/App.jsx
@@ -3,52 +3,44 @@ import { NavBar } from "./Components/NavBar/Navbar"
 import { InfoList } from "./Components/HabitList/InfoList";
 import { TodayHabit } from "./Components/LeftSide/TodayHabit";
 
-function getDate(){
+function getDate() {
   const today = new Date();
   return today.toLocaleDateString('en-US')
 }
 
-const App = () =>{
+const App = () => {
 
-  const [habits, setHabits] = useState(()=>{
+  const [habits, setHabits] = useState(() => {
     const stored = localStorage.getItem("habits");
     return stored ? JSON.parse(stored) : []
   })
 
-  useEffect(()=>{
-    localStorage.setItem("habits",JSON.stringify(habits));
-  },[habits])
+  useEffect(() => {
+    localStorage.setItem("habits", JSON.stringify(habits));
+  }, [habits])
 
-  function createHabit(inp, catg){
+  function createHabit(inp, catg) {
 
     const habit = {
-      id : Date.now(),
-      name : inp,
+      id: Date.now().toString(),
+      name: inp,
       category : catg,
-      completed : false
+      completed: false,
+      streak: 0,
+      lastCompletedDate: null
     }
 
-    setHabits((prev)=> [...prev, habit])
+    setHabits((prev) => [...prev, habit])
 
   }
 
- const toggleHabit = (id) => {
-  console.log(id)
-  setHabits((prev) =>
-    prev.map((habit) =>
-      habit.id === id
-        ? { ...habit, completed: !habit.completed }
-        : habit
-    )
-  );
-  console.log(habits)
-};
 
-  function deleteHabit(id){
-    setHabits((prev) => prev.filter((habit)=> habit.id !== id));
+
+  function deleteHabit(id) {
+    setHabits((prev) => prev.filter((habit) => habit.id !== id));
   }
 
-  return(
+  return (
 
     <div className="w-full min-h-screen bg-[#e9f6fa] ">
 
@@ -56,13 +48,13 @@ const App = () =>{
 
       <div className="px-16 py-4">
         <h1 className="text-4xl font-bold text-[#1A365D] mb-1">Good Morning, Krishna!</h1>
-        <p className="ml-2 font-medium text-[#4A5568]">{ getDate()}</p>
+        <p className="ml-2 font-medium text-[#4A5568]">{getDate()}</p>
       </div>
 
       < InfoList />
 
       <div className="container px-16 py-6 w-full">
-        < TodayHabit habits={habits} createHabit={createHabit} deleteHabit={deleteHabit} toggleHabit={toggleHabit}/>
+        < TodayHabit habits={habits} createHabit={createHabit} deleteHabit={deleteHabit} toggleHabit={toggleHabit} />
       </div>
 
     </div>
--- habit-tracker/src/Components/LeftSide/HabitCard.jsx
+++ habit-tracker/src/Components/LeftSide/HabitCard.jsx
@@ -3,11 +3,12 @@ export const HabitCard = ({habits, deleteHabit, toggleHabit}) => {
         <div className="flex flex-col px-7 gap-3">
             {habits.map((habit, id)=>{
                 return (
-                    <div key={id} className={`flex items-center justify-between px-5 py-3 ${ habit.completed ? "bg-green-600 " : "bg-white"} align-middle shadow-lg rounded-xl`}>
+                    <div key={id} className={`flex items-center justify-between px-5 py-3 ${ habit.completed ? "bg-green-600  shadow-xl" : "bg-white shadow-lg"} align-middle  rounded-xl`}>
                 <div className="flex flex-col align-middle">
                     <h3 className="font-semibold text-xl text-[#1A365D]">{habit.name}</h3>
                     <span className="font-lite ml-3 text-[#4A5568]">{habit.category}</span>
                 </div>
+                <p className="font-bold text-lg">{habit.streak} day streak</p>
                 <div className="flex gap-8 items-center justify-between">
                     <button className={`${habit.completed ? " hidden" :"bg-red-600 px-2 py-1 rounded-lg font-bold text-white" }`}
                     onClick={()=>{
